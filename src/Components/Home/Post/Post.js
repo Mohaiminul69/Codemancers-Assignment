7a9d9b115efebeb6b4353ca7407f29c74337cedb
mohaiminul69 <mohaiminul69@gmail.com>
2021-12-06 23:02:02 +0600

    post structure change

--- src/Components/Home/Post/Post.js
+++ src/Components/Home/Post/Post.js
@@ -4,9 +4,13 @@ import "./post.css";
 const Post = ({ post }) => {
   const { postMessage, selectedGIf } = post;
   return (
-    <div className="postDiv my-3 w-50 p-3">
-      <h3 className="text-white text-capitalize mb-3">{postMessage}</h3>
+    <div className="postDiv my-3 w-50 p-3 d-flex justify-content-between">
       <img className="postImg rounded-3" src={selectedGIf} alt="" />
+      <div className="w-100">
+        <h3 className="ms-3 text-left text-white text-capitalize mb-3">
+          {postMessage}
+        </h3>
+      </div>
     </div>
   );
 };
