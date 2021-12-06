aed9d98b22dbaf406c6f49e9c1cff3002ed5ba09
mohaiminul69 <mohaiminul69@gmail.com>
2021-12-06 20:02:54 +0600

    gif field and input filed moved to post div

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -1,38 +1,12 @@
-import React, { useState } from "react";
-import { Container, Button } from "react-bootstrap";
-import GifBox from "./GifBox/GifBox";
+import React from "react";
+import { Container } from "react-bootstrap";
 import "./home.css";
+import PostDiv from "./PostDiv/PostDiv";
 
 const Home = () => {
-  const [showGifBox, setShowGifBox] = useState(false);
-  const [selectedGIf, setSelectedGIf] = useState("");
-
-  const handleShowGifBox = () => {
-    setSelectedGIf("");
-    if (selectedGIf) {
-      setShowGifBox(true);
-    } else {
-      setShowGifBox(!showGifBox);
-    }
-  };
-
-  const handleGifSelect = (gif) => {
-    setSelectedGIf(gif);
-  };
-
   return (
     <Container className="mt-5">
-      <Button onClick={handleShowGifBox} variant="dark">
-        GIF
-      </Button>
-      {showGifBox && !selectedGIf && (
-        <GifBox handleGifSelect={handleGifSelect} />
-      )}
-      {selectedGIf && (
-        <div className="selectedGif mt-3">
-          <img src={selectedGIf} alt="" />
-        </div>
-      )}
+      <PostDiv />
     </Container>
   );
 };
---
+++ src/Components/Home/PostDiv/PostDiv.js
@@ -0,0 +1,40 @@
+import React, { useState } from "react";
+import { Container, Button } from "react-bootstrap";
+import GifBox from "./GifBox/GifBox";
+import "./postDiv.css";
+
+const PostDiv = () => {
+  const [showGifBox, setShowGifBox] = useState(false);
+  const [selectedGIf, setSelectedGIf] = useState("");
+
+  const handleShowGifBox = () => {
+    setSelectedGIf("");
+    if (selectedGIf) {
+      setShowGifBox(true);
+    } else {
+      setShowGifBox(!showGifBox);
+    }
+  };
+
+  const handleGifSelect = (gif) => {
+    setSelectedGIf(gif);
+  };
+
+  return (
+    <Container className="mt-5">
+      <Button onClick={handleShowGifBox} variant="dark">
+        GIF
+      </Button>
+      {showGifBox && !selectedGIf && (
+        <GifBox handleGifSelect={handleGifSelect} />
+      )}
+      {selectedGIf && (
+        <div className="selectedGif mt-3">
+          <img src={selectedGIf} alt="" />
+        </div>
+      )}
+    </Container>
+  );
+};
+
+export default PostDiv;
